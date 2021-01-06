83f1eb42d76640d6989be3762f67fb93342824bf
fereber <ferenc@berdal.hu>
2021-01-06 16:39:32 +0100

    Update snippets.

--- wp_code-snippets.php
+++ wp_code-snippets.php
@@ -98,12 +98,13 @@ add_filter( 'wp_editor_set_quality', function( $arg ) {
 } );
 
 /**
- * Remove generator tags
+ * Hide version number
  *
- * Hide version number by removing generator meta tags.
+ * Hide version number by removing generator meta tags and HTML comments.
  */
 remove_action( 'wp_head', 'wp_generator' );
 remove_action( 'wp_head', 'wc_generator' );
+add_filter( 'wpseo_debug_markers', '__return_false' );
 
 /**
  * Stop the Heartbeat API
@@ -246,3 +247,49 @@ if ( ! isset( $content_width ) ) {
 if ( ! current_user_can( 'manage_options' ) ) {
 	add_filter( 'show_admin_bar', '__return_false' );
 }
+
+/**
+ * WP Login to WC My Account
+ *
+ * Redirects wp-login.php page to default WooCommerce "My account" page, IF WooCommerce is installed AND active.
+ * 
+ *   Version:    1.1.9
+ *   Author:     MachineITSvcs 
+ *   Author URI: https://www.machineitservices.com
+ */
+$network_plugins = apply_filters('active_plugins', get_site_option('active_sitewide_plugins'));
+$subsite_plugins = apply_filters('active_plugins', get_option('active_plugins'));
+
+if(!is_admin() && (!function_exists('get_blog_status') || function_exists('get_current_blog_id') && empty(get_blog_status(get_current_blog_id(), 'deleted'))) && ((!empty($subsite_plugins) && in_array('woocommerce/woocommerce.php', $subsite_plugins)) || (!empty($network_plugins) && array_key_exists('woocommerce/woocommerce.php', $network_plugins)))) add_action('init', function() {
+  $myaccount = wc_get_page_permalink('myaccount');
+  global $pagenow;
+  if(home_url() != $myaccount) {
+    $action_array = array_unique(array_merge(((array) apply_filters('woo_wp_login_actions', array())), array("logout", "rp", "resetpass", "resetpassword", "enter_recovery_mode")));
+    if('wp-login.php' == $pagenow && (!isset($_GET['action']) || !in_array($_GET['action'], $action_array)) && !isset($_REQUEST['interim-login'])) {
+      if(!empty($_GET['action']) && $_GET['action'] == "lostpassword") {
+        unset($_GET['action']);
+        wp_redirect($myaccount . strtok(wc_get_endpoint_url('lost-password'), '/') . ((http_build_query($_GET)) ? '?' . http_build_query($_GET) : "" ));
+        exit();
+      }  else {
+        $woo_wp_redirect = ((isset($_GET['redirect_to'])) ? 'redirect-to=' . strtok($_GET['redirect_to'], '?') : "" );
+        unset($_GET['redirect_to'],$_GET['loggedout'],$_GET['reauth']);
+        $woo_wp_combined_query = (($woo_wp_redirect) ? $woo_wp_redirect : "" ) . ((http_build_query($_GET)) ? (($woo_wp_redirect) ? '&' : "" ) . http_build_query($_GET) : "" );
+        wp_redirect($myaccount . (($woo_wp_combined_query) ? '?' . $woo_wp_combined_query : "" ));
+        exit();
+      }
+    }
+    if(isset($_GET['redirect-to'])) {
+      if((isset($_SERVER['HTTPS']) ? "https" : "http") . '://' . $_SERVER['HTTP_HOST'] . strtok($_SERVER['REQUEST_URI'], '?') == $myaccount && is_user_logged_in() ) {
+        $myaccount_redirect = $_GET['redirect-to'];
+        unset($_GET['redirect-to'],$_GET['reauth']);
+        wp_redirect($myaccount_redirect . ((http_build_query($_GET)) ? '?' . http_build_query($_GET) : "" ));
+        exit();
+      } elseif((isset($_SERVER['HTTPS']) ? "https" : "http") . '://' . $_SERVER['HTTP_HOST'] . strtok($_SERVER['REQUEST_URI'], '?') != $myaccount) {
+        unset($_GET['redirect-to'],$_GET['reauth']);
+        wp_redirect((isset($_SERVER['HTTPS']) ? "https" : "http") . '://' . $_SERVER['HTTP_HOST'] . strtok($_SERVER['REQUEST_URI'], '?') . ((http_build_query($_GET)) ? '?' . http_build_query($_GET) : "" ));
+        exit();
+      }
+    }
+  }
+});
+
